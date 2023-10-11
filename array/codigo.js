9d7aae7f4cfc1fd9e8a96567cc9ace2fe09b633e
Guilherme <132853331+GuiBMA@users.noreply.github.com>
2023-10-11 15:01:04 -0300

    Aula JS

---
+++ array/codigo.js
@@ -0,0 +1,20 @@
+const ingrid = atletas[0];
+
+
+window.onload = () => {
+const espaco_nome = document.getElementById('nome');
+const espaco_imagem = document.querySelector('div#container > img');
+const espaco_descricao = document.querySelector('div#container > p');
+
+espaco_nome.innerText = ingrid.nome;
+espaco_imagem.src = ingrid.imagem;
+espaco_descricao.innerHTML = ingrid.descricao;
+}
+
+const preenche = (atleta) => {
+    espaco_nome.innerText = atleta.nome;
+    espaco_imagem.src = atleta.imagem;
+    espaco_descricao.innerHTML = atleta.descricao;
+}
+
+preenche(atletas[1]);
